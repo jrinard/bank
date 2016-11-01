ee0c86a4395303918b1b426cb0fe555130c5bcf7
Joshua Rinard and Jonathan Thom <jonathan@>
2016-11-01 13:55:17 -0700

    Initial commit. Basic functions with constructors and prototypes

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+function BankAccount(name, deposit) {
+  this.userName = name;
+  this.balance = deposit;
+}
+
+BankAccount.prototype.withdraw = function(withdrawalAmount) {
+  var balance = this.balance - withdrawalAmount;
+  return balance;
+}
+
+BankAccount.prototype.deposit = function(depositAmount) {
+  var balance = this.balance + depositAmount;
+  return balance;
+}
+
+
+$(document).ready(function() {
+  $("form#create").submit(function(event) {
+    event.preventDefault();
+    var name = $("#userName").val();
+    var input = parseInt($("#deposit").val());
+
+    var initialBalance = new BankAccount(name, input);
+    var balance = initialBalance.balance;
+    console.log(balance);
+
+
+  $("#withdrawal-button").submit(function(event){
+    event.preventDefault();
+    var withdraw = parseInt($("#withdrawalAmount").val());
+    var output = initialBalance.withdraw(withdraw);
+    console.log(output);
+  });
+
+  $("#deposit-button").submit(function(event){
+    event.preventDefault();
+    var deposit = parseInt($("#depositAmount").val());
+    var output = initialBalance.deposit(deposit);
+    console.log(output);
+  });
+
+});
+  });
